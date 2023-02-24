53b00263feba286e34a6f398a3208443aab43ed1
ssah0002 <sipra.sahu@synchronoss.com>
2023-02-24 23:44:21 +0530

    res

---
+++ src/main/resources/com/TP/FPT913_SEND/variables.java
@@ -0,0 +1,14 @@
+package com.TP.FPT913_SEND;
+
+public class variables implements java.io.Serializable {
+static final long serialVersionUID = 1L;
+public variables(){}
+private com.wisor.brms.fpt.ReqTypeAct reqTypeAct;
+public com.wisor.brms.fpt.ReqTypeAct getReqTypeAct() {
+return reqTypeAct;
+}
+public void setReqTypeAct(com.wisor.brms.fpt.ReqTypeAct reqTypeAct){
+this.reqTypeAct=reqTypeAct;
+}
+
+}
